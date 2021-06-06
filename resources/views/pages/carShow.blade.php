ec331672c7aee2e12ad894433ed47997052dfeaa
nunzio <chiefa.nunzio@gmail.com>
2021-06-06 22:56:39 +0200

    if-else su 0 piloti

--- resources/views/pages/carShow.blade.php
+++ resources/views/pages/carShow.blade.php
@@ -1,25 +1,30 @@
 @extends('layouts.main-layout')
 @section('content')
     <ul>   
-            <li>      
-                CAR NAME => {{$car -> name}} <br>
-                
-                CAR MODEL => {{$car -> model}}  <br>
-                       
-                CAR KW=> {{$car -> kw}}   <br>   
-                  
-                CAR BRAND => {{$car -> brand -> name}}   
-            </li>      
-        </ul>
-   
-        <ol>
-            @foreach ($car -> pilots as $pilot)
-                <li>
-                    PILOT NAME =>{{$pilot -> name}} <br>   
-                    PILOT LASTNAME =>{{$pilot -> lastname}} <br>   
-                    PILOT NATIONALITY =>{{$pilot -> nationality}}
-                    
-                </li>
-            @endforeach
-        </ol>
+        
+        <li>      
+            CAR NAME => {{$car -> name}} <br>
+            
+            CAR MODEL => {{$car -> model}}  <br>
+            
+            CAR KW=> {{$car -> kw}}   <br>   
+            
+            CAR BRAND => {{$car -> brand -> name}}   
+        </li>      
+    </ul>
+    
+    @if (count($car -> pilots) > 0)
+    <ol>
+        @foreach ($car -> pilots as $pilot)
+        <li>
+            PILOT NAME =>{{$pilot -> name}} <br>   
+            PILOT LASTNAME =>{{$pilot -> lastname}} <br>   
+            PILOT NATIONALITY =>{{$pilot -> nationality}}      
+            
+        </li>
+        @endforeach
+    </ol>
+    @else
+        <h2>NON CI SONO PILOTI ASSEGNATI A QUESTA MACCHINA</h2>
+    @endif
 @endsection
